4b9ce34026aa0d0eb56095713bbe2d357ab2a55d
Nolan Eakins <sneakin@semanticgap.com>
2022-01-11 07:01:22 -0500

    Fixed for SBCL.

--- tests/run.lisp
+++ tests/run.lisp
@@ -1,9 +1,17 @@
 (require :asdf)
 (require :nolisp)
 
+(use-package :nolisp)
+
 (load "src/testing/nassert.lisp")
 (load "src/testing/assert-match.lisp")
-(load "tests/units.lisp")
+
+(defun load-test-units ()
+  (let ((testdir (namestring (first (directory "./")))))
+    (mapcar #'(lambda (p) (print p) (load p))
+            (directory "./tests/**/*-test.lisp"))))
+
+(load-test-units)
 
 (test-match)
 (test-range)
