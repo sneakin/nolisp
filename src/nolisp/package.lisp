bf5ef38b38ce9f7409a1a86d78d58275c37b12d1
Nolan Eakins <sneakin@semanticgap.com>
2022-02-05 19:15:42 -0500

    Exporting using keywords which hopefully fixes the symbol conflict.

--- src/nolisp/package.lisp
+++ src/nolisp/package.lisp
@@ -1,14 +1,14 @@
 (defpackage :nolisp
   (:use :cl)
-  (:export fix-improper-list fix-improper-list! improper-mapcar flatten clip-last nshift-left shift-left shift-right reduce-values
-	   ifeq compose partial-first partial-after
-	   match
-	   lerp
-	   frange range
-	   scanner-error scan-list scan-list-reducer-fn scan-list-reducer
-	   error-msg error-form error-state error
-	   *macros* macro? add-macro remove-macro macro-expand-1 macro-expand
-	   lookup-resolver
-	   cps-transform
-	   *stages* compile-to-forth
-	   compile-to-lookup compile-to-list compile-to-flatlist to-string compile-to-string))
+  (:export :fix-improper-list :fix-improper-list! :improper-mapcar :flatten :clip-last :nshift-left :shift-left :shift-right :reduce-values
+	   :ifeq :compose :partial-first :partial-after
+	   :match
+	   :lerp
+	   :frange :range
+	   :scanner-error :scan-list :10scan-list-reducer-fn :scan-list-reducer
+	   :error-msg :error-form :error-state :error
+	   :*macros* :macro? :add-macro :remove-macro :macro-expand-1 :macro-expand
+	   :lookup-resolver
+	   :cps-transform
+	   :*stages* :compile-to-forth
+	   :compile-to-lookup :compile-to-list :compile-to-flatlist :to-string :compile-to-string))
